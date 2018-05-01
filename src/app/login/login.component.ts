e5b8bbe00e317934793d799aaf9b5bdb641f3d99
naphattharawat <naphattharawat@gmail.com>
2018-05-01 09:21:55 +0700

    ปรับ redirect การ login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../services/login.service';
 import { AlertService } from '../services/alert.service';
+import { JwtHelper } from 'angular2-jwt';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'cm-login',
@@ -13,10 +15,31 @@ export class LoginComponent implements OnInit {
   username: any;
   password: any;
   isLogging: boolean = false;
+  jwtHelper: JwtHelper = new JwtHelper();
+  token: string;
   
-  constructor(private router: Router, private loginService: LoginService, private alertService: AlertService) { }
+  constructor(private router: Router, private loginService: LoginService, private alertService: AlertService) { 
+  this.token = sessionStorage.getItem('token');
+  }
 
   ngOnInit() {
+    if (this.token) {
+      const decodedToken = this.jwtHelper.decodeToken(this.token);
+      const accessRight = decodedToken.accessRight;
+
+      try {
+        const rights = accessRight.split(',');
+
+        if (_.indexOf(rights, 'BM_ADMIN') > -1) {
+          this.router.navigate(['apps']);
+        } else {
+          this.alertService.error('ไม่มีสิทธิ์ในการเข้าถึง กรุณาลองไหม่');
+          this.router.navigate(['/login']);
+        }
+      } catch (error) {
+        this.router.navigate(['/login']);
+      }
+    }
   }
 
   enterLogin(event: any) {
@@ -32,9 +55,17 @@ export class LoginComponent implements OnInit {
         let rs: any = await this.loginService.doLogin(this.username, this.password);
         this.isLogging = false;
         if (rs.ok) {
-          let token: any = rs.token || null;
+          const token: any = rs.token || null;
+          const decodedToken = this.jwtHelper.decodeToken(token);
           sessionStorage.setItem('token', rs.token);
-          this.router.navigate(['/apps']);
+          const accessRight = decodedToken.accessRight;
+          const rights = accessRight.split(',');
+
+          if (_.indexOf(rights, 'CM_ADMIN') > -1) {
+            this.router.navigate(['/apps']);
+          } else {
+            this.alertService.error('ไม่มีสิทธิ์ในการเข้าถึง กรุณาลองไหม่');
+          }
         } else {
           this.alertService.error(rs.error);
         }
